fed914c1223d8d6158392ac4c83ab823e9b98583
Santi Mera <santigomero@gmail.com>
2024-07-18 12:37:35 +0300

    Added auth.routes, route-guard.middleware, added the SECRET to .env, added User.model

---
+++ server/models/User.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+        },
+        passwordHash: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`
+        timestamps: true,
+    }
+)
+
+const User = model('User', userSchema)
+
+module.exports = User
---
+++ server/routes/auth.routes.js
@@ -0,0 +1,59 @@
+const User = require('../models/User.model')
+const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken")
+const { isAuthenticated } = require('../middleware/route-guard.middleware')
+
+const router = require('express').Router()
+// All routes starts with /auth
+router.get('/', (req, res) => {
+    res.json('All good in auth')
+})
+
+// POST Signup
+router.post('/signup', async (req, res, next) => {
+    const salt = bcrypt.genSaltSync(13)
+    const passwordHash = bcrypt.hashSync(req.body.password, salt)
+    try {
+        const newUser = await User.create({ ...req.body, passwordHash })
+        res.status(201).json(newUser)
+    } catch (error) {
+        if (error.code === 11000) {
+            console.log("duplicate")
+        }
+        next(error)
+    }
+})
+// POST Login
+
+router.post("/login", async (req, res, next) => {
+    const { username, password } = req.body;
+    // try to get the user
+    // Check the password
+
+    try {
+        const potentialUser = await User.findOne({ username })
+
+        // User does exist with this username
+        if (potentialUser) {
+            // User has correct credentials
+            if (bcrypt.compareSync(password, potentialUser.passwordHash)) {
+                const token = jtw.sign({ userId: potentialUser._id }, process.env.TOKEN_SECRET)
+                res.json({ token })
+            } else {
+                res.status(403).json({ message: "Incorrect password" })
+            }
+        } else {
+            res.status(404).json({ message: "Username or password is not correct" })
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+// GET Verify
+
+router.get("/verify", isAuthenticated, (req, res, next) => {
+
+})
+
+module.exports = router
